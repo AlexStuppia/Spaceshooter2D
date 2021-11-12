7c23b99e0c28f141186068a398a115f0ad2739f2
AlexStuppia <alexstuppia20@gmail.com>
2021-11-12 13:48:00 +0100

    2D SpaceShooter V0.1
    My first game, currently in development for training purposes

---
+++ Assets/GameManager.cs
@@ -0,0 +1,107 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    public int punti;
+
+    public int playerEnergy;
+
+    public int nemici;
+
+    public int maxNemici;
+
+    public Text testoPunti;
+
+    public Text testoEnergia;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        punti = 0;
+        nemici = 0;
+        maxNemici = 10;
+        playerEnergy = 100;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void aggiungiPunti(int nuoviPunti)
+    {
+        punti = punti + nuoviPunti;
+
+        testoPunti.text = "Punti: " + punti;
+    }
+
+    public void aumentaNemici()
+    {
+        nemici++;
+    }
+
+    public void diminuisciNemici()
+    {
+        nemici--;
+    }
+
+    public void aumentaEnergia(int nuovaEnergia)
+    {
+        playerEnergy = playerEnergy + nuovaEnergia;
+
+        testoEnergia.text = "Energia: " + playerEnergy;
+    }
+
+    public void diminuisciEnergia(int energiaPersa)
+    {
+        playerEnergy = playerEnergy - energiaPersa;
+
+        testoEnergia.text = "Energia: " + playerEnergy;
+    }
+
+    public bool ilGiocatoreEVivo()
+    {
+        if (playerEnergy > 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+        
+    }
+
+    public bool possoMandareUnNuovoNemico()
+    {
+        if(nemici < maxNemici)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
